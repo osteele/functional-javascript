2cc054954953e1af2db283041cdb9f81e71333b6
Oliver Steele <steele@osteele.com>
2007-07-14 12:03:37 -0400

    +osdoc.tests.js

---
+++ osdoc.doctest.js
@@ -0,0 +1,74 @@
+/* 
+ * Author: Oliver Steele
+ * Copyright: Copyright 2007 by Oliver Steele.  All rights reserved.
+ * License: MIT License
+ * Source: http://osteele.com/javascripts/osdoc.doctest.js
+ * Created: 2007-07-13
+ * Modified: 2007-07-14
+ *
+ * Inspired by Tim Peter's wonderful doctest for Python.
+ */
+
+OSDoc.toString = function(value) {
+    if (value instanceof Array) {
+        var spans = map(OSDoc.toString, value);
+        return '[' + spans.join(', ') + ']';
+    }
+    switch (typeof(value)) {
+    case 'function': return 'function()';
+    case 'string': return '"' + value + '"';
+    case 'undefined': return 'undefined';
+    default: return value.toString();
+    }
+}
+
+OSDoc.Docs.prototype.runTests = function() {
+    var tests = [];
+    var failures = [];
+    this.records.each(function(defn) {
+        defn.tests.each(function(test) {
+            tests.push(test);
+            try {
+                var result = eval(test.text);
+            } catch (e) {
+                failures.push({defn: defn, test: test, error: e});
+                return;
+            }
+            if (test.expect != undefined && OSDoc.toString(result) != test.expect)
+                failures.push({defn: defn, test: test, result: result});
+        });
+    });
+    var lines = [];
+    failures.each(function(failure) {
+        var message = (failure.error
+                       ? [failure.test.text, ' throws ', failure.error]
+                       : [failure.test.text, ' -> ', toString(failure.result), ' != ', failure.test.expect]).join('');
+        lines.push(failure.defn.name + ': ' + message);
+    });
+    return this.testResults = {
+        tests: tests,
+        failure: failures,
+        success: !failures.length,
+        toHTML: function() {
+            return (failures.length
+                    ? ['Failed', failures.length, 'out of', tests.length, 'tests:\n'+lines.join('\n')]
+                    : ['Passed all', tests.length, 'tests.']).join(' ');
+        }
+    };
+}
+
+OSDoc.Docs.prototype.createTestText = function() {
+    var lines = [];
+    this.records.each(function(defn) {
+        defn.tests.length && lines.push('// ' + defn.name);
+        defn.tests.each(function(test) {
+            if (test.expect) {
+                lines.push('console.info(' + test.text.toString() + ');');
+                lines.push(['assertEquals(', test.expect, ', ', test.text, ');'].join(''));
+            } else
+                lines.push(test.text);
+        });
+        defn.tests.length && lines.push('');
+    });
+    return lines.join('\n').replace(/^/mg, '    ');
+}
--- osdoc.js
+++ osdoc.js
@@ -36,69 +36,6 @@ OSDoc.Docs.prototype.replace = function(target) {
     return this;
 }
 
-OSDoc.Docs.prototype.runTests = function() {
-    function toString(value) {
-        if (value instanceof Array) {
-            var spans = map(toString, value);
-            return '[' + spans.join(', ') + ']';
-        }
-        switch (typeof(value)) {
-        case 'function': return 'function()';
-        case 'string': return '"' + value + '"';
-        case 'undefined': return 'undefined';
-        default: return value.toString();
-        }
-    }
-    var tests = [];
-    var failures = [];
-    this.records.each(function(defn) {
-        defn.tests.each(function(test) {
-            tests.push(test);
-            try {
-                var result = eval(test.text);
-            } catch (e) {
-                failures.push({defn: defn, test: test, error: e});
-                return;
-            }
-            if (test.expect != undefined && toString(result) != test.expect)
-                failures.push({defn: defn, test: test, result: result});
-        });
-    });
-    var lines = [];
-    failures.each(function(failure) {
-        var message = (failure.error
-                       ? [failure.test.text, ' throws ', failure.error]
-                       : [failure.test.text, ' -> ', toString(failure.result), ' != ', failure.test.expect]).join('');
-        lines.push(failure.defn.name + ': ' + message);
-    });
-    return this.testResults = {
-        tests: tests,
-        failure: failures,
-        success: !failures.length,
-        toHTML: function() {
-            return (failures.length
-                    ? ['Failed', failures.length, 'out of', tests.length, 'tests:\n'+lines.join('\n')]
-                    : ['Passed all', tests.length, 'tests.']).join(' ');
-        }
-    };
-}
-
-OSDoc.Docs.prototype.createTestText = function() {
-    var lines = [];
-    this.records.each(function(defn) {
-        defn.tests.length && lines.push('// ' + defn.name);
-        defn.tests.each(function(test) {
-            if (test.expect) {
-                lines.push('console.info(' + test.text.toString() + ');');
-                lines.push(['assertEquals(', test.expect, ', ', test.text, ');'].join(''));
-            } else
-                lines.push(test.text);
-        });
-        defn.tests.length && lines.push('');
-    });
-    return lines.join('\n').replace(/^/mg, '    ');
-}
-
 OSDoc.Docs.prototype.toHTML = function(string) {
     var spans = [];
     var self = this;
